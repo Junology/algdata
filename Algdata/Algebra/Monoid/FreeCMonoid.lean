6b5821bcb13d36b437fc316fe28144e9aaf483f9
Junology <jtachan@hotmail.com>
2022-12-19 13:59:13 +0900

    Add license notice to FreeCMonoid.lean.

--- Algdata/Algebra/Monoid/FreeCMonoid.lean
+++ Algdata/Algebra/Monoid/FreeCMonoid.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2022 Jun Yoshida. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
 import Algdata.Init.LawfulLT
 import Algdata.Data.List.Prop
 import Algdata.Data.List.Ascending
