3725455f062a5ab395e3950b76a428e3e60ecf8c
Junology <jtachan@hotmail.com>
2022-12-19 15:48:15 +0900

    global import.

--- Algdata.lean
+++ Algdata.lean
@@ -3,4 +3,8 @@ Copyright (c) 2022 Jun Yoshida. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
+import Algdata.Algebra.Monoid.FreeCMonoid
+import Algdata.Algebra.Polynomial.MonomialOrder
+import Algdata.Algebra.Polynomial.Polynomial
+
 def hello := "world"
