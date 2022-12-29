8c64524938b8e8ff56b767feebef7d80dbc9d4af
Junology <jtachan@hotmail.com>
2022-12-30 01:08:46 +0900

    Fix the version of Std4.

--- lakefile.lean
+++ lakefile.lean
@@ -13,6 +13,8 @@ lean_lib Algdata {
 meta if get_config? doc = some "on" then
 require «dog-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
+require std from git "https://github.com/leanprover/std4" @ "bf3acbb"
+/-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-
+-/
