60620bd5a8e4bc6755eefea1b9cd7812281aaccc
Junology <jtachan@hotmail.com>
2022-12-30 00:56:50 +0900

    Add doc-gen4.

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,14 @@ package algdata {
   -- add any package configuration options here
 }
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
-
 @[default_target]
 lean_lib Algdata {
   -- add any library configuration options here
 }
+
+meta if get_config? doc = some "on" then
+require «dog-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
