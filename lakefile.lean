85199da0b8fa2968f2900c43c5bb4dad0dd840c2
Junology <jtachan@hotmail.com>
2023-01-02 20:41:56 +0900

    Add dependency to Dijkstra library.

--- lakefile.lean
+++ lakefile.lean
@@ -14,6 +14,8 @@ meta if get_config? doc = some "on" then
 require «dog-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 require std from git "https://github.com/leanprover/std4" @ "bf3acbb"
+
+require dijkstra from git "git@github.com:Junology/dijkstra.git" @ "master"
 /-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
