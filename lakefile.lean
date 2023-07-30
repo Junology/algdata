599d420167e2779aab43effc19462ed82f8ca65a
Junology <jtachan@hotmail.com>
2023-07-30 13:29:59 +0900

    chore: bump to nightly-2023-07-29

--- lakefile.lean
+++ lakefile.lean
@@ -13,7 +13,8 @@ lean_lib Algdata {
 meta if get_config? doc = some "on" then
 require «dog-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
-require std from git "https://github.com/leanprover/std4" @ "main"
+-- Use a commit with `chore: bump to nightly-YYYY-MM-DD` commit message.
+require std from git "https://github.com/leanprover/std4" @ "7601c54efadd70b688a163f5dcc11ae0ccdf7621"
 
 require dijkstra from git "git@github.com:Junology/dijkstra.git" @ "master"
 /-
