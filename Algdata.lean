11144251285727f45c150fcbfe0940ce22568420
Junology <jtachan@hotmail.com>
2023-09-10 14:47:20 +0900

    chore: add some files to the root file

--- Algdata.lean
+++ Algdata.lean
@@ -3,13 +3,17 @@ Copyright (c) 2022 Jun Yoshida. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
+import Algdata.Classes.ArrayLike
+import Algdata.Classes.UInt
+import Algdata.Data.Array.Lemmas
+import Algdata.Data.Array.Sized
+import Algdata.Data.ContainerSub.Defs
+import Algdata.Data.Matrix.Basic
+import Algdata.Data.Matrix.Instances
+import Algdata.Data.Matrix.Pretty
 import Algdata.Algebra.Monoid.FreeCMonoid
 import Algdata.Algebra.Polynomial.MonomialOrder
 import Algdata.Algebra.Polynomial.Polynomial
 import Algdata.Algebra.UIntAdic2.Basic
-import Algdata.Data.Array.Sized
-import Algdata.Data.Matrix.Basic
-import Algdata.Data.Matrix.Instances
-import Algdata.Data.Matrix.Pretty
 
 def hello := "world"
